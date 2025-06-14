325bd3b688084323c0b03efeecf63ca7edcf7c6a
Felipe Diaz Bertero <felipediazb16@gmail.com>
2025-06-14 10:26:54 -0300

    Boton de bloquear

--- clases/control/GestorRegResRevManual.py
+++ clases/control/GestorRegResRevManual.py
@@ -2,6 +2,8 @@ class GestorRegResRevManual:
     def __init__(self):
         # Lista que almacena los eventos sísmicos
         self.eventos = []
+        # Lista que almacena las instancias de Estado posible
+        self.lista_estados = []
 
     def cargarEventos(self, lista_eventos):
         """
@@ -13,8 +15,18 @@ class GestorRegResRevManual:
             raise TypeError("Se esperaba una lista de eventos sísmicos.")
 
         for evento in lista_eventos:
-            # Se puede validar tipo aquí si se desea
             self.eventos.append(evento)
+
+    def setEstados(self, lista_estados):
+        """
+        Carga una lista de objetos Estado al gestor.
+
+        :param lista_estados: list de Estado
+        """
+        if not isinstance(lista_estados, list):
+            raise TypeError("Se esperaba una lista de estados.")
+
+        self.lista_estados = lista_estados
 
     def buscarEventoSismicoAD(self):
         """
@@ -25,9 +37,14 @@ class GestorRegResRevManual:
         return self.eventos
 
     def cambiarEstadoEvento(self, evento, nombre_estado):
+        """
+        Cambia el estado del evento al estado con el nombre especificado.
+
+        :param evento: EventoSismico a modificar
+        :param nombre_estado: str con el nombre del nuevo estado
+        """
         for estado in self.lista_estados:
             if estado.nombre == nombre_estado:
                 evento.estado = estado
                 return
         raise ValueError(f"No se encontró el estado con nombre '{nombre_estado}'")
-
